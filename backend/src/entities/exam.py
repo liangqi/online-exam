7454ab2df5972f5dd2c917dd78eeb0fff94a8a87
Liang Qi <cavendish.qi@gmail.com>
2020-04-23 17:42:41 +0200

    integrating Flask RESTful endpoints and SQLAlchemy

--- backend/src/entities/exam.py
+++ backend/src/entities/exam.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from marshmallow import Schema, fields
+
 from sqlalchemy import Column, String
 
 from .entity import Entity, Base
@@ -15,3 +17,11 @@ class Exam(Entity, Base):
         Entity.__init__(self, created_by)
         self.title = title
         self.description = description
+
+class ExamSchema(Schema):
+    id = fields.Number()
+    title = fields.Str()
+    description = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
